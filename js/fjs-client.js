0db16ccc5fbc1260f31926f472af927b2a6cdc9c
jiapengdeng <18392622932@163..com>
2015-06-30 23:17:06 +0800

    先设想语法

---
+++ js/fjs-client.js
@@ -0,0 +1,10 @@
+/*支持类似ejs的写法<%%>,现在用{}代替*/
+/*第一种情况*/
+var temple='<p>name1</p><p>name2</p><p>name4</p>';
+
+alert(temple);
+function FjsClient(){
+    this.parse=function(temple,[{name:1},{name:2},{mame:4}]){
+        
+    }
+}
